5d04e83d63670cd4e800e65b7f9dbe78df678321
Student <Student@C105_25>
2026-03-10 08:59:26 +0100

    Form

--- Business_Card/Form1.Designer.cs
+++ Business_Card/Form1.Designer.cs
@@ -29,13 +29,245 @@ namespace Business_Card
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.dateTimePickerBirth = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxPosition = new System.Windows.Forms.ComboBox();
+            this.numericUpDownExperience = new System.Windows.Forms.NumericUpDown();
+            this.radioButtonFullTime = new System.Windows.Forms.RadioButton();
+            this.radioButtonPartTime = new System.Windows.Forms.RadioButton();
+            this.checkBoxRemote = new System.Windows.Forms.CheckBox();
+            this.checkBoxDriver = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownExperience)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(85, 14);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(207, 20);
+            this.textBoxFirstName.TabIndex = 0;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(85, 41);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(207, 20);
+            this.textBoxLastName.TabIndex = 1;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(85, 68);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(207, 20);
+            this.textBoxEmail.TabIndex = 2;
+            // 
+            // dateTimePickerBirth
+            // 
+            this.dateTimePickerBirth.Location = new System.Drawing.Point(85, 95);
+            this.dateTimePickerBirth.Name = "dateTimePickerBirth";
+            this.dateTimePickerBirth.Size = new System.Drawing.Size(207, 20);
+            this.dateTimePickerBirth.TabIndex = 3;
+            // 
+            // comboBoxPosition
+            // 
+            this.comboBoxPosition.FormattingEnabled = true;
+            this.comboBoxPosition.Location = new System.Drawing.Point(416, 13);
+            this.comboBoxPosition.Name = "comboBoxPosition";
+            this.comboBoxPosition.Size = new System.Drawing.Size(207, 21);
+            this.comboBoxPosition.TabIndex = 4;
+            // 
+            // numericUpDownExperience
+            // 
+            this.numericUpDownExperience.Location = new System.Drawing.Point(416, 41);
+            this.numericUpDownExperience.Name = "numericUpDownExperience";
+            this.numericUpDownExperience.Size = new System.Drawing.Size(207, 20);
+            this.numericUpDownExperience.TabIndex = 5;
+            // 
+            // radioButtonFullTime
+            // 
+            this.radioButtonFullTime.AutoSize = true;
+            this.radioButtonFullTime.Location = new System.Drawing.Point(416, 72);
+            this.radioButtonFullTime.Name = "radioButtonFullTime";
+            this.radioButtonFullTime.Size = new System.Drawing.Size(63, 17);
+            this.radioButtonFullTime.TabIndex = 6;
+            this.radioButtonFullTime.TabStop = true;
+            this.radioButtonFullTime.Text = "Full-time";
+            this.radioButtonFullTime.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonPartTime
+            // 
+            this.radioButtonPartTime.AutoSize = true;
+            this.radioButtonPartTime.Location = new System.Drawing.Point(507, 72);
+            this.radioButtonPartTime.Name = "radioButtonPartTime";
+            this.radioButtonPartTime.Size = new System.Drawing.Size(66, 17);
+            this.radioButtonPartTime.TabIndex = 7;
+            this.radioButtonPartTime.TabStop = true;
+            this.radioButtonPartTime.Text = "Part-time";
+            this.radioButtonPartTime.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxRemote
+            // 
+            this.checkBoxRemote.AutoSize = true;
+            this.checkBoxRemote.Location = new System.Drawing.Point(416, 101);
+            this.checkBoxRemote.Name = "checkBoxRemote";
+            this.checkBoxRemote.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxRemote.TabIndex = 8;
+            this.checkBoxRemote.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDriver
+            // 
+            this.checkBoxDriver.AutoSize = true;
+            this.checkBoxDriver.Location = new System.Drawing.Point(416, 128);
+            this.checkBoxDriver.Name = "checkBoxDriver";
+            this.checkBoxDriver.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxDriver.TabIndex = 9;
+            this.checkBoxDriver.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "First name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Last name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Date of birth";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(330, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Remote work";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(330, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Work mode";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(330, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Experience";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(330, 21);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Position";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(330, 129);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Driver license";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(641, 449);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBoxDriver);
+            this.Controls.Add(this.checkBoxRemote);
+            this.Controls.Add(this.radioButtonPartTime);
+            this.Controls.Add(this.radioButtonFullTime);
+            this.Controls.Add(this.numericUpDownExperience);
+            this.Controls.Add(this.comboBoxPosition);
+            this.Controls.Add(this.dateTimePickerBirth);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownExperience)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBirth;
+        private System.Windows.Forms.ComboBox comboBoxPosition;
+        private System.Windows.Forms.NumericUpDown numericUpDownExperience;
+        private System.Windows.Forms.RadioButton radioButtonFullTime;
+        private System.Windows.Forms.RadioButton radioButtonPartTime;
+        private System.Windows.Forms.CheckBox checkBoxRemote;
+        private System.Windows.Forms.CheckBox checkBoxDriver;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
